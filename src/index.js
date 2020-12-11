d9c31bbff27b07a6bcfd91f6db45d56773a64bf2
Lucas Frois <lucas.frois@lambda3.com.br>
2020-12-11 16:16:36 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './main/Calculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Calculadora</h1>      
+      <Calculator />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
